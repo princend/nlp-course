9e069f0ae7157c448aa97e644f525bc70ca59875
Princend <waxdji3g4y3c04@gmail.com>
2024-12-03 01:36:35 +0800

    使用中文model

--- l1.py
+++ l1.py
@@ -21,12 +21,32 @@ def zero_shot_classification(
     print(zeroshotanswer)
 
 
-def text_generation(text="In this course, we will teach you how to"):
-    generator = pipeline("text-generation")
-    res= generator(text)    
-    print(res)    
-    
-    
+class TextGeneratorBase:
+    def __init__(self, model, max_length=30, num_return_sequences=2):
+        self.generator = pipeline("text-generation", model=model)
+        self.max_length = max_length
+        self.num_return_sequences = num_return_sequences
+
+    def generate_text(self, text):
+        result = self.generator(
+            text,
+            max_length=self.max_length,
+            num_return_sequences=self.num_return_sequences,
+        )
+        print(result)
+
+class EnglishTextGenerator(TextGeneratorBase):
+    def __init__(self, model="distilgpt2"):
+        super().__init__(model=model)
+
+class ChineseTextGenerator(TextGeneratorBase):
+    def __init__(self, model="uer/gpt2-chinese-cluecorpussmall"):
+        super().__init__(model=model)
+
+
+
 # sentiment_analysis()
 # zero_shot_classification()
-text_generation()
+# EnglishTextGenerator().generate_text("I've been waiting for a HuggingFace course my whole life.")
+ChineseTextGenerator().generate_text("在這門課程中，我們將教你如何")
+
