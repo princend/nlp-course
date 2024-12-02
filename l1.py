7562b02c99832e744b4eb2814937fd8458cc0ce9
Princend <waxdji3g4y3c04@gmail.com>
2024-12-03 01:20:52 +0800

    變成fn

--- l1.py
+++ l1.py
@@ -1,13 +1,24 @@
 from transformers import pipeline
 
-classifier = pipeline("sentiment-analysis")
-a= classifier("I've been waiting for a HuggingFace course my whole life.")
-print(a)
 
-# zeroshot= pipeline("zero-shot-classification")
-# zeroshotanswer=classifier(
-#     "This is a course about the Transformers library",
-#     candidate_labels=["education", "politics", "business"],
-# )
+def sentiment_analysis(
+    text="I've been waiting for a HuggingFace course my whole life.",
+):
+    classifier = pipeline("sentiment-analysis")
+    a = classifier(text)
+    print(a)
 
-# print(zeroshotanswer)
+
+def zero_shot_classification(
+    text="This is a course about the Transformers library",
+    canditate_labels=["education", "politics", "business"],
+):
+    zeroshot = pipeline("zero-shot-classification")
+    zeroshotanswer = zeroshot(
+        text,
+        candidate_labels=canditate_labels,
+    )
+    print(zeroshotanswer)
+    
+    
+zero_shot_classification()
