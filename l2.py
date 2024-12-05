871cd5d4f0026ce8c586e84ce58de7acfa7e9169
Princend <waxdji3g4y3c04@gmail.com>
2024-12-06 01:08:12 +0800

    lesson 2

--- l2.py
+++ l2.py
@@ -66,9 +66,100 @@ def inference():
     model=BertModel.from_pretrained("bert-base-cased")
     output = model(model_inputs)
     print(output)
+
+def tokenize():
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
+
+    sequence = "Using a Transformer network is simple"
+    tokens = tokenizer.tokenize(sequence)
+
+    print(tokens)    
+    ids = tokenizer.convert_tokens_to_ids(tokens)
+
+    print(ids)
+
+def decoding():
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
+    decoded_string = tokenizer.decode([7993, 170, 11303, 1200, 2443, 1110, 3014])
+    print(decoded_string)   
+
+def batch_input():
+    checkpoint = "distilbert-base-uncased-finetuned-sst-2-english"
+    tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+    model = AutoModelForSequenceClassification.from_pretrained(checkpoint)
+
+    sequence = "I've been waiting for a HuggingFace course my whole life."
+
+    tokens = tokenizer.tokenize(sequence)
+    ids = tokenizer.convert_tokens_to_ids(tokens)
+    input_ids = torch.tensor([ids])
+    print("Input IDs:", input_ids)
+    output=model(input_ids)   
+    print("Logits:", output.logits)
+    
+def pad_token():
+    checkpoint = "distilbert-base-uncased-finetuned-sst-2-english"
+    tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+    model = AutoModelForSequenceClassification.from_pretrained(checkpoint)
+
+    sequence1_ids = [[200, 200, 200]]
+    sequence2_ids = [[200, 200]]
+    batched_ids = [
+        [200, 200, 200],
+        [200, 200, tokenizer.pad_token_id],
+    ]
+
+    print(model(torch.tensor(sequence1_ids)).logits)
+    print(model(torch.tensor(sequence2_ids)).logits)
+    print(model(torch.tensor(batched_ids)).logits)    
+
+def attension_mask():
+    checkpoint = "distilbert-base-uncased-finetuned-sst-2-english"
+    tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+    model = AutoModelForSequenceClassification.from_pretrained(checkpoint)
+    
+    batched_ids = [
+    [200, 200, 200],
+    [200, 200, tokenizer.pad_token_id],
+    ]
+
+    attention_mask = [
+        [1, 1, 1],
+        [1, 1, 0],
+    ]
+
+    outputs = model(torch.tensor(batched_ids), attention_mask=torch.tensor(attention_mask))
+    print(outputs.logits) 
+    
+def truncation():
+    sequences = ["I've been waiting for a HuggingFace course my whole life.", "So have I!"]
+
+    # Will truncate the sequences that are longer than the model max length
+    # (512 for BERT or DistilBERT)
+    model_inputs = tokenizer(sequences, truncation=True)
+
+    # Will truncate the sequences that are longer than the specified max length
+    model_inputs = tokenizer(sequences, max_length=8, truncation=True) 
+    
+def ending():
+    checkpoint = "distilbert-base-uncased-finetuned-sst-2-english"
+    tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+    model = AutoModelForSequenceClassification.from_pretrained(checkpoint)
+    sequences = ["I've been waiting for a HuggingFace course my whole life.", "So have I!"]
+
+    tokens = tokenizer(sequences, padding=True, truncation=True, return_tensors="pt")
+    output = model(**tokens)
+    print(output)
+    
 # classfication()
 # tokenizer()
 # automodel_for_seq_classification()
 # set_bert_config()
 # get_pretrained_model()
-inference()
+# inference()
+# tokenize()
+# decoding()
+# batch_input()
+# pad_token()
+# attension_mask()
+ending()
