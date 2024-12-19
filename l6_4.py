64c4602a8f82100bc98dcc7d651543a99d30cbb7
Princend <waxdji3g4y3c04@gmail.com>
2024-12-19 23:56:45 +0800

    stride

--- l6_4.py
+++ l6_4.py
@@ -6,4 +6,24 @@ context = """
 between them. It's straightforward to train your models with one before loading them for inference with the other.
 """
 question = "Which deep learning libraries back 🤗 Transformers?"
-question_answerer(question=question, context=context)
+answer = question_answerer(question=question, context=context)
+
+print(answer)
+
+from transformers import AutoTokenizer, AutoModelForQuestionAnswering
+
+model_checkpoint = "distilbert-base-cased-distilled-squad"
+tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+model = AutoModelForQuestionAnswering.from_pretrained(model_checkpoint)
+
+inputs = tokenizer(question, context, return_tensors="pt")
+outputs = model(**inputs)
+
+
+sentence = "This sentence is not too long but we are going to split it anyway."
+inputs = tokenizer(
+    sentence, truncation=True, return_overflowing_tokens=True, max_length=6, stride=2
+)
+
+for ids in inputs["input_ids"]:
+    print(tokenizer.decode(ids))
